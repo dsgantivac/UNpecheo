3999b802c0199333285c55265925ed142d476bc5
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-09-15 17:59:11 -0500

    delete graphql

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,33 +1,7 @@
 var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-
 var Order = require('./mongo');
 var pool = require('./pg');
 
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-var root = { hello: () => {
-    var o;
-    await Order.findOne({id:8779887}).then(function (err, order) {
-        if(err){
-            console.log(err);
-            return;
-        }
-        o = order;
-    });
-    console.log(o);
-    return o;
-} };
-
 var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
