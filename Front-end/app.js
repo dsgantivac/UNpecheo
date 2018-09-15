557db00aed600d85b561ccb6326f53179cf8e99d
David Perdomo <daeperdomocr@unal.edu.co>
2018-09-15 21:57:38 +0000

    front-end map and geolocator

--- Front-end/app.js
+++ Front-end/app.js
@@ -1,62 +1,52 @@
-const init_coord=[4.624335,-74.063644]
+/*
+*/
+var init_coord=[4.624335,-74.063644]
 
-function htmlbodyHeightUpdate(){
-	var height3 = $( window ).height()
-	var height1 = $('.nav').height()+50
-	height2 = $('.main').height()
-	if(height2 > height3){
-		$('html').height(Math.max(height1,height3,height2)+10);
-		$('body').height(Math.max(height1,height3,height2)+10);
-	}
-	else
-	{
-		$('html').height(Math.max(height1,height3,height2));
-		$('body').height(Math.max(height1,height3,height2));
-	}
-	
+/*
+test data
+*/
+
+/*----*/
+function getGeotext(){
+    var input = document.getElementById("geohash-input").value;
+console.log
+  $.ajax({
+    url:'https://maps.googleapis.com/maps/api/geocode/json?address='+input+',NY',
+    dataType: 'json'
+    }).done(function(obj){
+     // console.log("data"+i,datus);
+      if(obj.status=="OK"){  
+        console.log("obj ",obj);
+        point = ({"lat": parseFloat(obj.results[0].geometry.location.lat), "lng": parseFloat(obj.results[0].geometry.location.lng)});
+       // console.log("lalpoint ",point)            
+      }
+    });
 }
-
+/*
+Map plot
+*/
 function myMap() {
     var centermap = new google.maps.LatLng(init_coord[0],init_coord[1]);
         
     map = new google.maps.Map(document.getElementById("googleMap"),{
         center:centermap,
-        zoom:14,
-        styles: [
-                {"featureType": "poi",
-                  "stylers": [{"visibility": "off"}]},
-                {"featureType": "poi.attraction",
-                  "elementType": "labels",
-                  "stylers": [{"visibility": "off"}]},
-                {"featureType": "poi.government",
-                  "elementType": "labels",
-                  "stylers": [{"visibility": "off"}]},
-                {"featureType": "poi.park",
-                  "stylers": [{"visibility": "on"}]}
-              ]
+        zoom:14
     });
-    try{
-    }
-    catch(err){
-        console.log(err)
-    }
+    
+    //Traffic layer 
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+    
 }
+
+/*-----*/
 window.onload = function(){
   run();
 }
 function run(){
   myMap();
 }
+
 $(document).ready(function() {
   //run(); 
-  $("#showall").on("click", myMap);
-  
-    htmlbodyHeightUpdate()
-    $( window ).resize(function() {
-    	htmlbodyHeightUpdate()
-    });
-    $( window ).scroll(function() {
-    	height2 = $('.main').height()
-      	htmlbodyHeightUpdate()
-    });
 })
