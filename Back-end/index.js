f6f149d53c705354c77d843af9c651bdf8ce4f10
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-09-16 12:03:56 -0500

    aaaaa

--- Back-end/index.js
+++ Back-end/index.js
@@ -29,4 +29,19 @@ app.get('/storekeeper/:date-:date2', function(req, res) {
     }) 
 });
 
+app.post('/zone/', function(req, res) {
+    pool.query("select * from storekeepers where timestamp between '" + req.params.date.replace('_', ' ') + "' and '" + req.params.date2.replace('_', ' ') + "'"
+        , (err, sk) => {
+            if(err){
+                console.log(err);
+                pool.end();
+                return;
+            }
+            console.log("SQL\n", sk);
+            res.json(sk);
+            
+            pool.end();  
+    }) 
+});
+
 app.listen(4000, () => console.log('Now browse to localhost:4000'));
