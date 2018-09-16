cf5695ff2e797ebb7eb5010337b22ccf72d681fc
David Gantiva <dsgantivac@gmail.com>
2018-09-16 00:40:43 -0500

    modificaciones front

--- Back-end/index.js
+++ Back-end/index.js
@@ -10,7 +10,6 @@ app.get('/orders/', function(req, res) {
         .exec(function(err, orders) {
             if (err)
                 res.send(err);
-
             res.json(orders);
         });
 });
@@ -19,15 +18,14 @@ app.get('/storekeeper/', function(req, res) {
     Order.find(function(err, orders) {
         if (err)
             res.send(err);
-
         pool.query('select * from storekeepers limit 5', (err, sk) => {
             console.log("SQL\n", err, sk);
             res.json(sk.row);
-            
+
             pool.end();
         })
-        
-    }); 
+
+    });
 });
 
 app.listen(4000, () => console.log('Now browse to localhost:4000'));
