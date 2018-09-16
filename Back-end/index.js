2151b85598e0918a5d6f63c17072e0701698a701
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-09-16 13:50:54 -0500

    Auto stash before merge of "master" and "origin/master"

--- Back-end/index.js
+++ Back-end/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Order = require('./schema/orders');
 var Storekeeper = require('./schema/storekeepers');
 var pool = require('./connection/pg');
-var pool = require('./connection/mongo');
+var mongo = require('./connection/mongo');
 
 var app = express();
 
@@ -14,8 +14,7 @@ app.get('/orders/', function(req, res) {
 var contain = require("@turf/boolean-contains");
 
 app.get('/orders/:date/:date2', function(req, res) {
-    var a = req.params.date2.replace('_', ' ');
-    var b = req.params.date.replace('_', ' ');
+
     Order.find(/*{ 
         "timestamp" : { 
           $lt: req.params.date2.replace('_', ' '), 
@@ -25,11 +24,18 @@ app.get('/orders/:date/:date2', function(req, res) {
 //        .limit(10)
 >>>>>>> 6a1d707a7025ae1e8a49b14bee3000ed8207d1a4
         .exec(function(err, orders) {
-            console.log(err,orders);
+            console.log(err);
             if (err)
                 res.send(err);
-            orders = orders.filter(function(order){
-
+            var t = new Date(order.timestamp);
+            var a = new Date(req.params.date)//req.params.date.replace('_', ' '));
+            var d = new Date(req.params.date2)//req.params.date2.replace('_', ' '));
+            
+            orders = orders.filter(function(order){           
+                if( t >= a && t <= d){
+                    return true;
+                }
+                return false;
             })
             res.json(orders);
         });
@@ -44,10 +50,10 @@ app.get('/storekeeper/:date/:date2', function(req, res) {
                 return;
             }
             console.log("SQL\n", sk);
-            res.json(sk);
-
-            pool.end();
-    })
+            res.json(sk.rows);
+            
+            pool.end();  
+    }) 
 });
 
 app.post('/storekeeper/zone/:date-:date2', function(req, res) {
