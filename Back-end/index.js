65e41154db07a683a6b2f03548bcec37d564b622
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-09-16 15:01:43 -0500

    Auto stash before merge of "master" and "origin/master"

--- Back-end/index.js
+++ Back-end/index.js
@@ -6,10 +6,27 @@ var mongo = require('./connection/mongo');
 
 var app = express();
 var contain = require("@turf/boolean-contains");
+var barrios;
+https.get('https://gist.githubusercontent.com/john-guerra/ee93225ca2c671b3550d62614f4978f3/raw/b1d556c39f3d7b6e495bf26b7fda815765ac110a/bogota_cadastral.json', (resp) => {
+  var data = '';
 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    barrios = JSON.parse(data);
+    console.log(data);
+    
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
 
 app.get('/orders/:date/:date2', function(req, res) {
-
     Order.find(/*{ 
         "timestamp" : { 
           $lt: req.params.date2.replace('_', ' '), 
@@ -21,19 +38,60 @@ app.get('/orders/:date/:date2', function(req, res) {
             console.log(err);
             if (err)
                 res.send(err);
-            var t = new Date(order.timestamp);
-            var a = new Date(req.params.date)//req.params.date.replace('_', ' '));
-            var d = new Date(req.params.date2)//req.params.date2.replace('_', ' '));
-            
+           
             orders = orders.filter(function(order){           
+                var t = new Date(order.timestamp); 
+                var a = new Date(req.params.date)//req.params.date.replace('_', ' '));
+                var d = new Date(req.params.date2)//req.params.date2.replace('_', ' '));
+                console.log(t);
+                
                 if( t >= a && t <= d){
                     return true;
                 }
                 return false;
             })
-            var o = {}
 
+            var o = {
+                
+            }
+            console.log(orders);
+            
             for(var i=0; i<orders.length; i++){
+                if(!o[orders[i].type]){
+                    o[orders[i].type] = [];
+                }
+                o[orders[i].type].push(orders[i]);
+            }
+            res.json(o);
+        });
+});
+
+app.get('/orders/zone/:date/:date2', function(req, res) {
+    Order.find()
+        .exec(function(err, orders) {
+            console.log(err);
+            if (err)
+                res.send(err);
+           
+            orders = orders.filter(function(order){           
+                var t = new Date(order.timestamp); 
+                var a = new Date(req.params.date)//req.params.date.replace('_', ' '));
+                var d = new Date(req.params.date2)//req.params.date2.replace('_', ' '));
+                console.log(t);
+                
+                if( t >= a && t <= d){
+                    return true;
+                }
+                return false;
+            })
+
+            var o = {
+                
+            }
+            console.log(orders);
+            
+            for(var i=0; i<orders.length; i++){
+                for(var j=0; j<)
                 if(!o[orders[i].type]){
                     o[orders[i].type] = [];
                 }
